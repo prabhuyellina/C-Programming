d372d6c1e4ba248d86231cf1edee5cc2689b9196
prabhuyellina <prabhuyellina@users.noreply.github.com>
2022-04-19 18:53:52 -0500

    Create Intersection_of_two_linked_list_final.c

---
+++ Intersection_of_two_linked_list_final.c
@@ -0,0 +1,24 @@
+/* Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns. */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    struct ListNode *p1=headA,*p2=headB;
+    while(p1!=p2){
+        p1= p1==NULL?headB:p1->next;
+        p2= p2==NULL?headA:p2->next;
+    }
+    return p1;
+}
